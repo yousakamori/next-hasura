15217ef186d9bad8da45e481a8d831a1b3f1f9ec
yousakamori <yousakamori@gmail.com>
2022-04-22 11:18:21 +0900

    apolloのclient取得処理を追加

---
+++ pages/hasura-main.tsx
@@ -0,0 +1,38 @@
+import { useQuery } from '@apollo/client'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import { Layout } from '../components/Layout'
+import { GET_USERS } from '../queries/queries'
+import { GetUsersQuery } from '../types/generated/graphql'
+
+const FetchMain: NextPage = () => {
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS)
+
+  if (error) {
+    return (
+      <Layout title="Hasura FetchPolicy">
+        <p>Error: {error.message}</p>
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout title="Hasura fetchPolicy">
+      <>
+        <p className="mb-6 font-bold">Hasura main page</p>
+        {console.log(data)}
+        {data?.users.map((user) => {
+          return (
+            <p className="my-1" key={user.id}>
+              {user.name}
+            </p>
+          )
+        })}
+        <Link href="/hasura-sub">
+          <a className="mt-6">Next</a>
+        </Link>
+      </>
+    </Layout>
+  )
+}
+export default FetchMain
